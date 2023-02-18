c78fcd51196c3964c914eaaae6b072422f73418c
CDong24 <71855459+CDong24@users.noreply.github.com>
2023-02-17 18:40:37 -0600

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -186,7 +186,7 @@ var config_data = `
         "2": "2<br>",
         "3": "3<br>",
         "4": "4<br>",
-        "5": "5"
+        "5": "5",
       },
       "defaultValue": "x"
     },
@@ -198,7 +198,7 @@ var config_data = `
         "2": "2<br>",
         "3": "3<br>",
         "4": "4<br>",
-        "5": "5 (fast)<br>",
+        "5": "5 (fast)",
       },
       "defaultValue":"3"
     },
