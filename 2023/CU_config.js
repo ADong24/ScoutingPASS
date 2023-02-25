feb6add770099e369af93d3d1fba01ac203f1d36
CDong24 <71855459+CDong24@users.noreply.github.com>
2023-02-24 19:40:18 -0600

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -154,7 +154,7 @@ var config_data = `
         "t": "Substation<br>",
         "g": "Ground<br>",
         "b": "Both<br>",
-        "x": "Not Attempted"
+        "x": "Failed to Collect"
       },
       "defaultValue": "x"
     }
