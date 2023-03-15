ea85f29cec1cf03f56f82e714fe9f788c6112ae9
ADong24 <71855495+ADong24@users.noreply.github.com>
2023-03-14 20:07:45 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "TXWAC",
+      "defaultValue": "TXFOR",
       "required": "true",
       "disabled": "true"
     },
