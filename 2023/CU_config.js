47e40541dafb67dac7c5a77494ec3819c097628b
ADong24 <71855495+ADong24@users.noreply.github.com>
2023-04-03 19:11:36 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "TXCMP",
+      "defaultValue": "TXAMA",
       "required": "true",
       "disabled": "true"
     },
