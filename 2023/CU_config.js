5bbae42418ad1dbee659181112b2e028c845aa7f
ADong24 <71855495+ADong24@users.noreply.github.com>
2023-04-12 19:47:05 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -189,6 +189,7 @@ var config_data = `
         "e": "Engaged<br>",
         "pi": "Parked in Community<br>",
         "po": "Parked outside Community<br>",
+        "f": "Failed to engage<br>",
         "x": "Not attempted"
       },
       "defaultValue": "x"
